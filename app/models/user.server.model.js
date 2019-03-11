516c9e8d1267458b01b6e33470356fd719168abc
Matthew Toohey <mct63@uclive.ac.nz>
2019-03-11 19:20:05 +1300

    Password is no longer stored in plain text

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -1,20 +1,32 @@
 const db = require('../../config/db');
+const bcrypt = require('bcrypt');
 
 exports.insert = function(username, email, given_name, family_name, password, done) {
-    let values = [username, email, given_name, family_name, password];
 
     if (!isValidEmail(email)) {
-        done(400, {"Validation Error": "Invalid email address"})
+        done(400, {"Validation Error": "Invalid email address"});
+    } else if (!isValidPassword(password)) {
+        done(400, {"Validation Error": "Invalid password"});
     } else {
+        let values = [username, email, given_name, family_name, hashPassword(password)];
+
         db.getPool().query('INSERT INTO User (username, email, given_name, family_name, password) VALUES ?', [[values]], function(err, result) {
             if (err) return done(400, err);
     
             done(201, {"userId": result.insertId});
         });
     }
-};
+}
 
 function isValidEmail(email) {
     var emailRe = /[a-zA-Z0-9!#$%&'*+-/=?^_`{|}~.]+@[a-zA-z0-9-.]+/;
     return emailRe.test(email);
+}
+
+function isValidPassword(password) {
+    return password.length > 0;
+}
+
+function hashPassword(password) {
+    return bcrypt.hashSync(password, 12);
 }
