bea2af83fdf1462bb4be8b3112f1d42431d9ef65
Matthew Toohey <mct63@uclive.ac.nz>
2019-03-14 14:51:08 +1300

    Added check for valid category id for post venues endpoint

--- app/models/venue.server.model.js
+++ app/models/venue.server.model.js
@@ -12,11 +12,22 @@ exports.insert = function(venueName, categoryId, city, shortDescription, longDes
             let adminId = rows[0].user_id;
             let date = new Date(Date.now()).toISOString();
             let values = [venueName, categoryId, city, shortDescription, longDescription, address, latitude, longitude, adminId, date];
-            db.getPool().query('INSERT INTO Venue (venue_name, category_id, city, short_description, long_description, address, latitude, longitude, admin_id, date_added) VALUES ?', [[values]], function(err, result) {
+            db.getPool().query('SELECT * FROM VenueCategory WHERE category_id = ?', categoryId, function(err, rows) {
                 if (err) {
-                    done(400, err);
+                    done(500, err);
+                } else if (rows.length === 0) {
+                    // TODO: Check reference server response.
+                    done(400, {"ERROR": "Venue category id should be unique"});
+                } else if (rows.length > 1) {
+                    done(500, err);
                 } else {
-                    done(201, {"venueId": result.insertId});
+                    db.getPool().query('INSERT INTO Venue (venue_name, category_id, city, short_description, long_description, address, latitude, longitude, admin_id, date_added) VALUES ?', [[values]], function(err, result) {
+                        if (err) {
+                            done(400, err);
+                        } else {
+                            done(201, {"venueId": result.insertId});
+                        }
+                    });
                 }
             });
         }
